7ea89bb3fdfbbb2a8e40da5c1018daf1558b5104
akshat <akshat2004july@gmail.com>
2025-07-05 12:58:51 +0530

    latest

--- connect front and b/backend/server.js
+++ connect front and b/backend/server.js
@@ -3,7 +3,7 @@ import express from 'express'
 const app=express()
 
 app.get('/', (req, res) =>{
-    res.send("namaste world")
+    res.send("hello world")
 })
 
 const port = process.env.PORT || 3000;
